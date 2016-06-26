47535df1c5de691ef0a8ea7ee274ddabb42449ba
Shen Tian <biozinc@gmail.com>
2016-06-26 19:31:04 +0200

    cleaning up

--- examples/cloud/CloudsSketch.java
+++ examples/cloud/CloudsSketch.java
@@ -6,11 +6,15 @@
 import processing.core.*;
 import me.lsdo.processing.*;
 
-public class CloudsSketch extends PointSampleSketch<CloudsState> {
+public class CloudsSketch extends PointSampleSketch {
 
-    CloudsState state;
-    double last_t;
     int mode;
+    
+    double dx;
+    double dy;
+    double dz;
+    
+    double last_t;
 
     double noiseScale=0.02;
 
@@ -18,48 +22,25 @@ public class CloudsSketch extends PointSampleSketch<CloudsState> {
         super(app, dome, opc);
         mode = 0;
         
-        app.colorMode(HSB,255);
+        dx = dy = dz = 0;
+        
+        app.colorMode(app.HSB,255);
     }
 
-    protected CloudsState initialState() {
-        return new CloudsState();
-    }
-
-    protected CloudsState updateState(CloudsState state, double delta_t) {
-        double t = last_t + delta_t;
+    protected void preFrame(double t) {
+        double delta_t = t - last_t;
+        last_t = t;
+        
         double speed = 0.06;
         double zspeed = 3.;
         double angle = Math.sin(t);
 
-        state.dx += Math.cos(angle) * speed * delta_t;
-        state.dy += Math.sin(angle) * speed * delta_t;
-        state.dz += (noise(t * 0.014) - 0.5) * zspeed * delta_t;
-        return state;
+        dx += Math.cos(angle) * speed * delta_t;
+        dy += Math.sin(angle) * speed * delta_t;
+        dz += (noise(t * 0.014) - 0.5) * zspeed * delta_t;
     }
 
     protected int samplePoint(PVector p, double t, double t_jitter) {
-        switch (mode) {
-        case 0:
-            return drawCloud(p, t);
-        case 1:
-            return drawRing(p, t);          
-        default:
-            throw new RuntimeException();
-        }
-    }
-    
-    void processKeyInput(){
-        if (app.keyPressed) {
-            if (app.key == 'c') {
-                mode = 0;
-            }
-            if (app.key == 'r') {
-                mode = 1;
-            }
-        }
-    }
-
-    private int drawCloud(PVector p, double t) {
         // Noise patterns are symmetrical around the origin and it looks weird. Move origin to corner.
         p = LayoutUtil.Vadd(p, LayoutUtil.V(1, 1));
 
@@ -67,39 +48,12 @@ public class CloudsSketch extends PointSampleSketch<CloudsState> {
         double hue = .1 * t;
         double scale = .75;
         
-        double n = fractalNoise(state.dx + p.x*scale, state.dy + p.y*scale, z) - 0.75;
-        double m = fractalNoise(state.dx + p.x*scale, state.dy + p.y*scale, z + 10.0) - 0.75;
+        double n = fractalNoise(dx + p.x*scale, dy + p.y*scale, z) - 0.75;
+        double m = fractalNoise(dx + p.x*scale, dy + p.y*scale, z + 10.0) - 0.75;
                     
         return app.color(
                      (int)(255 * MathUtil.fmod(hue + .8 * m, 1.)), 
                      (int)(255 * (1. - constrain(Math.pow(3.0 * n, 3.5), 0, 0.9))), 
-                     (int)(255 * constrain(Math.pow(3.0 * n, 1.5), 0, 0.9))
-                     );
-    }
-
-    private int drawRing(PVector p, double t) {
-        // Noise patterns are symmetrical around the origin and it looks weird. Move origin to corner.
-        p = LayoutUtil.Vadd(p, LayoutUtil.V(1, 1));
-
-        double z = .08 * t;
-        double hue = .1 * t;
-        double scale = .75;
-        
-        double saturation = constrain(Math.pow(1.15 * noise(t * 0.122), 2.5), 0, 1);
-        double spacing = noise(t * 0.124) * 15.;
-        
-        double centerx = noise(t *  0.125) * 2.5;
-        double centery = noise(t * -0.125) * 2.5;
-        
-        double dist = Math.sqrt(Math.pow(p.x - centerx, 2) + Math.pow(p.y - centery, 2));
-        double pulse = (Math.sin(state.dz + dist * spacing) - 0.3) * 0.3;
-                    
-        double n = fractalNoise(state.dx + p.x*scale + pulse, state.dy + p.y*scale, z) - 0.75;
-        double m = fractalNoise(state.dx + p.x*scale, state.dy + p.y*scale, z + 10.0) - 0.75;
-                    
-        return app.color(
-                     (int)(255 * MathUtil.fmod(hue + .4 * m, 1.)), 
-                     (int)(255 * saturation),
                      (int)(255 * constrain(Math.pow(3.0 * n, 1.5), 0, 0.9))
                      );
     }
--- examples/cloud/cloud.pde
+++ examples/cloud/cloud.pde
@@ -5,39 +5,16 @@
 import me.lsdo.processing.*;
 
 CloudsSketch cloud;
-Dome dome;
-OPC opc;
+
 void setup() {
     size(300, 300);
-    dome = new Dome();
-    opc = new OPC("127.0.0.1", 7890);
+    Dome dome = new Dome();
+    OPC opc = new OPC("127.0.0.1", 7890);
     cloud = new CloudsSketch(this, dome, opc);
-    
 }
 
 void draw() {
-  for (DomeCoord c : dome.coords){
-      
-      dome.setColor(c, cloud.samplePoint(dome.getLocation(c), millis()/1000d, 0));
-  }
-  
-      background(0);
-    noStroke();
-    for (DomeCoord c : dome.coords){
-        PVector p = LayoutUtil.xyToScreen(dome.getLocation(c), width, height, 2*dome.getRadius(), true);
-        fill(dome.getColor(c));
-           ellipse(p.x, p.y, 3, 3);
-        }
-     opc.draw();
-        
-    fill(128);
-    text("opc @" + opc.getHost(), 100, height - 10);
-    text(String.format("%.1ffps", frameRate), 10, height - 10);
-  
-}
-
-void keyPressed(){
-   //driver.processKeyInput();
+      cloud.draw(); 
 }
 
 
